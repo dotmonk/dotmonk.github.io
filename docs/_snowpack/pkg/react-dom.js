f0b1b62c2298867f269b584ab4c6a0e0fadd1dbc
Magnus Weinberg <magnus.weinberg@gmail.com>
2023-04-15 18:56:15 +0200

    update

---
+++ docs/_snowpack/pkg/react-dom.js
@@ -0,0 +1,1 @@
+import"./common/index-02c5f5cd.js";export{r as default}from"./common/index-02c5f5cd.js";import"./common/index-3d8027a6.js";
---
+++ docs/_snowpack/pkg/react.js
@@ -0,0 +1,1 @@
+import{r as o}from"./common/index-3d8027a6.js";export{r as default}from"./common/index-3d8027a6.js";var m=o.Component;export{m as Component};
